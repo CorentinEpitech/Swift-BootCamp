e98dcb01e2987b9b81bc84e959c13f4bc4922e05
Corentin ZILIO <52495672+CorentinEpitech@users.noreply.github.com>
2022-09-05 23:48:10 +0200

    [ + ] Existing projects

---
+++ Dicee/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceSet = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
+    @IBAction func rollButton(_ sender: UIButton) {
+        diceImageView1.image = diceSet.randomElement()
+        diceImageView2.image = diceSet[Int.random(in: 0 ..< 6)]
+    }
+}
+
